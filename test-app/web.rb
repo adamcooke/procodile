b2a241991ce916afe135f3965289a6dfca4387b7
Adam Cooke <me@adamcooke.io>
2016-10-16 20:23:01 +0100

    some updates to the test app

--- test-app/web.rb
+++ test-app/web.rb
@@ -1,7 +1,9 @@
 trap("TERM", proc { Process.exit(0) })
 
-puts "Web server running on port 5000"
+puts "Web server running on port 5000. \n Isn't this nice?\n This is on multiple lines."
 $stdout.flush
 loop do
-  sleep 5
+  puts "Something! #{Time.now.to_s}"
+  $stdout.flush
+  sleep 2
 end
--- test-app/worker.rb
+++ test-app/worker.rb
@@ -2,7 +2,12 @@ trap("TERM", proc { $exit = 1; puts("Exiting..."); $stdout.flush })
 
 puts "Working running"
 $stdout.flush
+count = 0
 loop do
+  count +=1
   sleep 2
+  if count >= 3
+    Process.exit(1)
+  end
   Process.exit(0) if $exit
 end
