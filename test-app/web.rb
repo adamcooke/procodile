6f9082ebbd3eb9a5876b66bb082078a937836251
Adam Cooke <me@adamcooke.io>
2016-10-18 14:54:48 +0100

    update web.rb to flush stdout when exiting

--- test-app/web.rb
+++ test-app/web.rb
@@ -1,4 +1,4 @@
-trap("TERM", proc { puts "Exiting..." ; sleep(2) ; Process.exit(0) })
+trap("TERM", proc { puts "Exiting..." ; $stdout.flush ; sleep(2) ; Process.exit(0) })
 
 puts "Web server running on port 5000. \n Isn't this nice?\n This is on multiple lines."
 $stdout.flush
