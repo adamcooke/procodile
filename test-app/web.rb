ceb66c860d03136182d73fe61404e1ff4eb3ecb7
Adam Cooke <me@adamcooke.io>
2016-10-18 22:09:48 +0100

    update README

--- test-app/web.rb
+++ test-app/web.rb
@@ -1,4 +1,4 @@
-trap("TERM", proc { puts "Exiting..." ; $stdout.flush ; sleep(2) ; Process.exit(0) })
+trap("TERM", proc { puts "Exiting..." ; $stdout.flush ; Process.exit(0) })
 
 puts "Web server running on port 5000. \n Isn't this nice?\n This is on multiple lines."
 $stdout.flush
@@ -7,7 +7,7 @@ puts "PID file: #{ENV['PID_FILE']}"
 puts "SMTP server: #{ENV['SMTP_HOSTNAME']}"
 puts "SMTP user: #{ENV['SMTP_USERNAME']}"
 loop do
-  puts "Something! #{Time.now.to_s}"
+  #puts "Something! #{Time.now.to_s}"
   $stdout.flush
   sleep 2
 end
